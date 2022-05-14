0b47c50edfb798a62e5fd2016bbb2fb015de992c
AnMado <qjrpcg@gmail.com>
2022-05-14 20:15:30 +0300

    All tests are ready

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -6,7 +6,9 @@ import org.junit.BeforeClass
 import org.junit.Assert.*
 
 import java.io.ByteArrayOutputStream
+import java.io.FileReader
 import java.io.PrintStream
+import java.math.BigInteger
 
 class MainTest {
 
@@ -33,20 +35,48 @@ class MainTest {
 
     @Before
     fun prepareTest() {
-        bytesOutput.flush()
+        bytesOutput.reset()
     }
 
     @After
     fun cleanupTest() {
-        bytesOutput.flush()
+        bytesOutput.reset()
     }
 
     @Test
-    fun testZeroFactorial() {
+    fun testFactorialOfZero() {
         main("0")
-        var actual = bytesOutput.toString(Charsets.UTF_8).trim()
+        var actual = bytesOutput.toString(Charsets.UTF_8).trim().toInt()
 
-        assertEquals("Fractal of zero is wrong", 1, actual.toInt())
+        assertEquals("Factorial of zero is wrong", 1, actual)
+    }
+
+    @Test
+    fun testFactorialOfOne(){
+        main("1")
+        var actual = bytesOutput.toString(Charsets.UTF_8).trim().toInt()
+
+        assertEquals("Factorial of one is wrong", 1, actual)
+    }
+
+    @Test
+    fun testNotBigFactorial(){
+        main("6")
+        var actual = bytesOutput.toString(Charsets.UTF_8).trim().toInt()
+
+        assertEquals("Factorial of small number is wrong", 720, actual)
+    }
+
+    @Test
+    fun testBigFactorial(){
+        main("100")
+        var actual = BigInteger(bytesOutput.toString(Charsets.UTF_8).trim())
+
+
+        var fileReader = FileReader("./src/test/answer/answer.txt")
+        var expected = BigInteger(fileReader.readLines()[0])
+
+        assertTrue("Factorial of big number is wrong", expected == actual)
     }
 
 
